ca00b6ddafdac1be6b807cecf05699795db40393
valeps <valepserna@gmail.com>
2026-06-12 11:53:23 -0500

    feat: implement product management crud and data infrastructure

--- src/Firmeza.Admin/Program.cs
+++ src/Firmeza.Admin/Program.cs
@@ -1,7 +1,47 @@
+using Firmeza.Admin.Data;
+using Firmeza.Admin.Interfaces;
+using Firmeza.Admin.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+// Protect all pages in the panel
+builder.Services.AddRazorPages(options =>
+{
+  options.Conventions.AuthorizeFolder("/", "AdminOnly");
+  options.Conventions.AllowAnonymousToPage("/Account/Login");
+});
+
+// Conection to DB
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+  options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Scopes
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IClientService, ClientService>();
+
+
+// Identity configuration 
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+  {
+    options.SignIn.RequireConfirmedAccount = false;
+  })
+  .AddRoles<IdentityRole>()
+  .AddEntityFrameworkStores<ApplicationDbContext>();
+
+// Configure the Identity redirect
+builder.Services.ConfigureApplicationCookie(options =>
+{
+  options.LoginPath = "/Account/Login";
+  options.AccessDeniedPath = "/Account/AccessDenied";
+});
+
+builder.Services.AddAuthorization(options =>
+{
+  options.AddPolicy("AdminOnly", policy => policy.RequireRole("Administrador"));
+});
 
 var app = builder.Build();
 
@@ -17,10 +57,14 @@ app.UseHttpsRedirection();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope())
+{
+  await DbSeeder.SeedAsync(scope.ServiceProvider);
+}
 
 app.Run();
